299b5bfb69f93e920e62280c37a26d9ed12bc86b
Daymere <imakheline@gmail.com>
2022-01-16 02:00:44 +0300

    add slider animation

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import {Swiper, SwiperSlide} from 'swiper/react';
 import {Autoplay} from 'swiper';
@@ -7,6 +7,14 @@ import PlusButton from '../PlusButton/PlusButton';
 import "./hero.scss";
 
 // Images
+import personaje01 from '@root/assets/images/hero/personaje-01.png';
+import personaje01Webp from '@root/assets/images/hero/personaje-01.webp';
+import personaje02 from '@root/assets/images/hero/personaje-02.png';
+import personaje02Webp from '@root/assets/images/hero/personaje-02.webp';
+import personaje03 from '@root/assets/images/hero/personaje-03.png';
+import personaje03Webp from '@root/assets/images/hero/personaje-03.webp';
+import personaje04 from '@root/assets/images/hero/personaje-04.png';
+import personaje04Webp from '@root/assets/images/hero/personaje-04.webp';
 import background from '@root/assets/images/hero/background.png';
 import backgroundWebp from '@root/assets/images/hero/background.webp';
 import backgroundMobile from '@root/assets/images/hero/background@mobile.png';
@@ -21,9 +29,18 @@ import item02 from '@root/assets/images/hero/item-02.png';
 import item02Webp from '@root/assets/images/hero/item-02.webp';
 import item03 from '@root/assets/images/hero/item-03.png';
 import item03Webp from '@root/assets/images/hero/item-03.webp';
+import item04 from '@root/assets/images/hero/item-04.png';
 import internetMade from '@root/assets/images/hero/internet-made.png';
 
+function setClassActive(index, current){
+    return index === current 
+        ? "background-personaje active"
+        : "background-personaje";
+};
+
 export default function(){
+    const [backgroundIndex, setBackgroundIndex] = useState(null);
+
     const settings = {
         slidesPerView: 3,
         loop: true,
@@ -34,11 +51,15 @@ export default function(){
         autoplay: {
             delay: 5000
         },
+        initialSlide: 0,
         speed: 1200,
         breakpoints: {
             992: {
                 direction: "vertical",
             }
+        },
+        onSlideChange(swiper){
+            setBackgroundIndex(swiper.realIndex);
         }
     };
 
@@ -49,7 +70,23 @@ export default function(){
                     <source srcSet={backgroundMobileWebp} media="(max-width: 768px)" type="image/webp" />
                     <source srcSet={backgroundMobile} media="(max-width: 768px)" type="image/png" />
                     <source srcSet={backgroundWebp} type="image/webp" />
-                    <img src={background} alt="background" />
+                    <img src={background} alt="background" className="background-tree" />
+                </picture>
+                <picture>                    
+                    <source srcSet={personaje01Webp} type="image/webp" />
+                    <img src={personaje01} alt="bg-personaje" className={setClassActive(0, backgroundIndex)} />
+                </picture>
+                <picture>           
+                    <source srcSet={personaje02Webp} type="image/webp" />         
+                    <img src={personaje02} alt="bg-personaje" className={setClassActive(1, backgroundIndex)} />
+                </picture>
+                <picture>             
+                    <source srcSet={personaje03Webp} type="image/webp" />       
+                    <img src={personaje03} alt="bg-personaje" className={setClassActive(2, backgroundIndex)} />
+                </picture>
+                <picture>          
+                    <source srcSet={personaje04Webp} type="image/webp" />          
+                    <img src={personaje04} alt="bg-personaje" className={setClassActive(3, backgroundIndex)} />
                 </picture>
             </div>
             <div className="hero__container">
@@ -98,27 +135,35 @@ export default function(){
                     </div>
                     <div className="hero__center">                        
                         <Swiper className="hero__swiper" {...settings}>
-                            <SwiperSlide>
+                            <SwiperSlide data-image-src="/assets/images/hero/item-01">
                                 <div className="hero__slider-item">
                                     <picture>
-                                        <source srcSet={item01Webp} type="image/webp" />                                        
+                                        {/* <source srcSet={item01Webp} type="image/webp" />                                         */}
                                         <img src={item01} alt="item" />
                                     </picture>
                                 </div>
                             </SwiperSlide>
-                            <SwiperSlide>
+                            <SwiperSlide data-image-src="/assets/images/hero/item-02">
                                 <div className="hero__slider-item">
                                     <picture>
-                                        <source srcSet={item02Webp} type="image/webp" />                                        
+                                        {/* <source srcSet={item02Webp} type="image/webp" />                                         */}
                                         <img src={item02} alt="item" />
                                     </picture>
                                 </div>
                             </SwiperSlide>
-                            <SwiperSlide>
+                            <SwiperSlide data-image-src="/assets/images/hero/item-03">
                                 <div className="hero__slider-item">
                                     <picture>
-                                        <source srcSet={item03Webp} type="image/webp" />                                        
+                                        {/* <source srcSet={item03Webp} type="image/webp" />                                         */}
                                         <img src={item03} alt="item" />
+                                    </picture>
+                                </div>
+                            </SwiperSlide>
+                            <SwiperSlide data-image-src="/assets/images/hero/item-04">
+                                <div className="hero__slider-item">
+                                    <picture>
+                                        {/* <source srcSet={item04Webp} type="image/webp" />                                         */}
+                                        <img src={item04} alt="item" />
                                     </picture>
                                 </div>
                             </SwiperSlide>
